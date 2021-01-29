7e83d3764bc435b472cc6765723c096bfbf889ab
Ruveyda H. AKÇINAR <rhakcinar@gmail.com>
2021-01-30 00:09:04 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+
+        {
+            Console.WriteLine("Tebrikler. Müşteri kaydınız oluşturuldu. : " + musteri.Id + "  " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.TelefonNo);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri kaydınız silindi :" + musteri.Id + "  " + musteri.Adi + "  " + musteri.Soyadi + " " + musteri.TelefonNo);
+        }
+
+
+        public void List (Musteri musteri)
+        {
+
+             Console.WriteLine("Tebrikler. İstenilen müsteriler listelendi :" + musteri.Id + "  " + musteri.Adi + "  " + musteri.Soyadi + " " + musteri.TelefonNo);
+        
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ClassMetotDemo
+  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 32565874513;
+            musteri1.Adi = "Ruveyda";
+            musteri1.Soyadi = "Başarı";
+            musteri1.TelefonNo = 05369874112;
+
+            
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 12114658256;
+            musteri2.Adi = "Arda";
+            musteri2.Soyadi = "Toprak";
+            musteri2.TelefonNo = 02321101447;
+
+            
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 36655441239;
+            musteri3.Adi = "Halime";
+            musteri3.Soyadi = "Sağlam";
+            musteri3.TelefonNo = 05462131014;
+
+            
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.TelefonNo);
+                Console.WriteLine("\n");
+
+
+            }
+            Console.WriteLine("---------------------------------");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+            Console.WriteLine("---------------------------------");
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.Delete(musteri3);
+
+            Console.WriteLine("---------------------------------");
+            musteriManager.List(musteri1);
+            musteriManager.List(musteri2);
+            musteriManager.List(musteri3);
+
+            
+        }
+    }
+}
